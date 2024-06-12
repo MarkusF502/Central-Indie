312b8adab8f0d5a6d2639eb2030878572ff9b4a8
ACN NORTE <markusfilipe502@gmail.com>
2024-06-11 22:44:14 -0300

    Central indie versão final

---
+++ scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        dots: true,
+        arrows: true
+    });
+});
